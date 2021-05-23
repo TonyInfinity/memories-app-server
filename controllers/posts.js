ebad9866b87eeadd449443ba2015932d3868ccaf
TonyInfinity <quachhengtony@gmail.com>
2021-05-23 18:56:29 +0700

    Done MERN CRUD.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,6 @@
+import express from "express";
 import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
     try {
@@ -19,4 +21,36 @@ export const createPost = async (req, res) => {
     } catch(error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post for that id");
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+    res.json(updatedPost);
+
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+    console.log("DELETE!");
+    res.json({ message: "Post deleted" });
+  
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that id");
+
+    const post = await PostMessage.findById(id); 
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    res.json(updatedPost);
 }
--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -12,11 +14,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://tonyquach:tonyquach123@cluster0.el4rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, ({ useNewUrlParser: true, useUnifiedTopology: true }))
-    .then(() => app.listen(PORT, () => console.log("Server is running on port 5000")))
+mongoose.connect(process.env.CONNECTION_URL, ({ useNewUrlParser: true, useUnifiedTopology: true }))
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
     .catch(error => console.log(error.message));
 
 mongoose.set("useFindAndModify", false);
